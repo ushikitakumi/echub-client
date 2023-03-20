eaca00571d44336508641877a428203c032aca07
ushikitakumi <cowki.1999@gmail.com>
2023-03-20 20:31:24 +0900

    商品画像の表示

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useRef, useState } from 'react';
 import './App.css';
-import ApiFetch from './components/ApiFetch';
+import ApiFetch from './components/ApiFetch'
 
 function App() {
+
+  const [fetchData, setFetchData] = useState([])
+  const ref = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(ref.current.value)
+
+    // APIをたたく
+    const endpointURL = `http://127.0.0.1:8000/api/${ref.current.value}/`
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        setFetchData(data)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <ApiFetch />
+      <h2>EChub</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="商品を検索" ref={ref}/>
+      </form>
+      <ApiFetch fetchData={fetchData}/>
     </div>
   );
 }
--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const ApiFetch = () => {
+const ApiFetch = ({ fetchData }) => {
   return (
     <div>
-      
+        <div>
+            {fetchData.map((data) => (
+                <div className='image'>
+                    <a href={data.url} target="_blank" rel="noopener noreferrer">
+                        <img src={data.image} alt="画像を表示できません"/>
+                    </a>
+                </div>
+            ))}
+
+        </div>
     </div>
   )
 }
