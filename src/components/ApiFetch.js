47f686ad5b60a3316f3ba36ff174090eda0b95e2
ushikitakumi <cowki.1999@gmail.com>
2023-03-30 17:07:47 +0900

    商品画像の整列

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,20 +1,75 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
 
 const ApiFetch = ({ fetchData }) => {
-  return (
-    <div>
-        <div>
-            {fetchData.map((data) => (
-                <div className='image'>
-                    <a href={data.url} target="_blank" rel="noopener noreferrer">
-                        <img src={data.image} alt="画像を表示できません"/>
-                    </a>
-                </div>
-            ))}
+    return (
+      <Container>
+        {fetchData.map((data) => (
+          <Image key={data.id}>
+            <ImageWrapper href={data.url} target="_blank" rel="noopener noreferrer">
+              <ImageContent src={data.image} alt="画像を表示できません" />
+            </ImageWrapper>
+            <Info>
+              <Name>{data.name}</Name>
+              <Price>{data.price}</Price>
+            </Info>
+          </Image>
+        ))}
+      </Container>
+    );
+  };
+  
+  export default ApiFetch;
 
-        </div>
-    </div>
-  )
-}
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
-export default ApiFetch
+const Image = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+  width: 300px;
+`;
+
+const ImageWrapper = styled.a`
+  display: block;
+  width: 100%;
+  height: 300px;
+  overflow: hidden;
+`;
+
+const ImageContent = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Name = styled.p`
+  margin: 0;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Price = styled.p`
+  margin: 0;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+`;
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import SearchBar from './SearchBar';
 const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className='container-fluid'>
-        <div className="navbar-brand">EChub</div>
+      <div className="container-fluid d-flex align-items-center justify-content-center">
+        <div className="navbar-brand me-4">EChub</div>
         <SearchBar handleSubmit={props.handleSubmit}/>
       </div>
     </nav>
